c8e1afff62ec5a81c27ddb44e6aef632fe107509
TheosXenos <blueonion@proton.me>
2022-10-18 15:48:12 +0200

    Complete assignment: Foundations Fundamentals 1

---
+++ foundations-fundamentals-part-1/script.js
@@ -0,0 +1,20 @@
+// Part 4:
+let a = 10;
+
+// Part 5:
+const MAX = 57;
+
+let actual = MAX - 13;
+let percentage = actual / MAX;
+
+console.log("Part 1:");
+console.log(23 + 97);
+
+console.log("===\nPart 2:");
+console.log(1+2+3+4+5+6);
+
+console.log("===\nPart 3:");
+console.log((4 + 6 + 9) / 77);
+
+console.log("===\nPart 4:");
+console.log(a);
