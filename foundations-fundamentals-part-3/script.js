13691c06648654219e130ca08afd2cdda841fbe1
TheosXenos <blueonion@proton.me>
2022-10-27 16:03:39 +0200

    Complete exercises

---
+++ foundations-fundamentals-part-3/script.js
@@ -0,0 +1,21 @@
+function add7(number)
+{
+    return number + 7;
+}
+
+function multiply(x,y)
+{
+    return x * y;
+}
+
+function capitalize(string)
+{
+    string = string[0].toUpperCase() + string.substring(1).toLowerCase();
+    
+    return string;
+}
+
+function lastLetter(string)
+{
+    return string.substring(string.length - 1);
+}
